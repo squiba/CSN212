0419a1e4dfcb80bd8c9a311b478b5981984b77cf
squiba <bhupendrajnv5@gmail.com>
2017-02-28 03:04:51 +0530

    minor change

--- Assignment2/problem3.py
+++ Assignment2/problem3.py
@@ -1,13 +1,26 @@
-""" 
+'''
 The Lucky Draw
 Longest Increasing Subsequence
 https://www.codechef.com/problems/D2/ 
 
-"""
+'''
+def long_increasing_subseq(A,index,N):
+    dp=[1 for i in range(N)]
+    for i in range(index+1,index+N):
+        for j in range(index,i):
+            if A[j]<A[i]:
+                dp[i-index]=max(dp[i-index],dp[j-index])
+    return max(dp[i-index])
+
+
+    
 def main():
     no_of_tests = int(raw_input())
-    for i in range(no_of_tests):
+    for test in range(no_of_tests):
         N = int(raw_input())
         a = map(int,raw_input().strip().split())
 
-def 
+        answer = 0
+        for i in range(N):
+            answer = max(answer,long_increasing_subseq(a,i,N))
+            
