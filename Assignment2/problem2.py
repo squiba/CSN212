5423a81097ec357cf9fd2b9b7628a63c9a5a08b8
squiba <bhupendrajnv5@gmail.com>
2017-02-27 22:31:48 +0530

    moved interval tree files into the directory and commited dp problem2

---
+++ Assignment2/problem2.py
@@ -0,0 +1,34 @@
+def banned_roads(banned_strings):
+    banned_paths = {}
+    for banned_string in banned_strings:
+        banned_coords = [int(item) for item in banned_string.split()]
+        banned_coords = [tuple(banned_coords[:2]), tuple(banned_coords[2:])]
+        banned_coords.sort()  # we always move towards larger coords
+        src, dst = banned_coords
+        banned_srcs = banned_paths.setdefault(dst, [])
+        banned_srcs.append(src)
+    return banned_paths
+
+def avoid_roads(rows, cols, banned_paths):
+    rows += 1
+    cols += 1
+    prev_row = [1] + [0] * (cols - 1)
+    for row in range(rows):
+        this_row = []
+        before = 0
+        for col, above in enumerate(prev_row):
+            banned_srcs = banned_paths.get((row, col), [])
+            if (row, col - 1) in banned_srcs:
+                before = 0
+            if (row - 1, col) not in banned_srcs:
+                before += above
+            this_row.append(before)
+        prev_row = this_row
+    return prev_row[-1]
+
+
+print avoid_roads(6, 6, banned_roads(['0 0 0 1', '6 6 5 6']))
+print avoid_roads(1, 1, {})
+print avoid_roads(35, 31, {})
+print avoid_roads(6, 6, banned_roads(['0 0 1 0', '1 2 2 2',
+                                      '1 1 2 1']))
---
+++ Assignment2/problem3.py
@@ -0,0 +1,12 @@
+""" 
+https://www.codechef.com/problems/D2/ 
+python2 
+
+"""
+def main():
+    no_of_tests = int(raw_input())
+    for i in range(no_of_tests):
+        N = int(raw_input())
+        a = map(int,raw_input().strip().split())
+
+def 
